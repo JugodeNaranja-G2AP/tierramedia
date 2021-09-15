56d10d72358e83efe63d61d569274a3c6207ce85
Rosana Cohen <55336881+rocohen@users.noreply.github.com>
2021-09-15 14:06:47 -0300

    Agrega tests para la clase Usuario

--- test/tierramedia/UsuarioTests.java
+++ test/tierramedia/UsuarioTests.java
@@ -33,5 +33,31 @@ public class UsuarioTests {
 		int costoEsperado = 10;
 		assertEquals(costoEsperado, usuarioPrueba.obtenerCostoTotalItinerario(), 0);
 	}
+	
+	@Test
+	public void presupuestoTest() {
+		int presupuestoEsperado = 2;
+		assertEquals(presupuestoEsperado, usuarioPrueba.getPresupuesto());
+		
+	}
+	
+	@Test
+	public void tiempoDisponibleTest() {
+		double tiempoEsperado = 3;
+		assertEquals(tiempoEsperado, usuarioPrueba.getTiempoDisponible(), 0);
+	}
+	
+	@Test
+	public void comproElProductoTest() {
+		assertTrue(usuarioPrueba.comproElProducto(atraccion1));
+		assertTrue(usuarioPrueba.comproElProducto(atraccion2));
+		assertFalse(usuarioPrueba.comproElProducto(atraccion3));
+	}
+	
+	@Test
+	public void obtenerNombresdeProductosCompradosTest() {
+		String nombresEsperados = "Minas Tirith\nAbismo de Helm\n";
+		assertEquals(nombresEsperados, usuarioPrueba.obtenerNombresdeProductosComprados());
+	}
 
 }
